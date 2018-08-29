3b1d06e6d6ea6483f5d30510bb8ae4cf988b0fc9
kartiksr <kartiksrivastava144@gmail.com>
2018-08-29 15:53:28 +0800

    Added file to predict the missing word based on context
    Description:
    * Added context_fill.py that takes context_fill.txt file in data folder and predicts the word that should be filled in the blank based on the previous words (left context)
    * Also added evaluation method for the task that blanks out a particular word in the sentence randomly and then the model tries to predict the word. Accuracy is measured on the set of output words.
    * Added context_data.py that makes the dataset for context filling task

--- context_fill.py
+++ context_fill.py
@@ -1,9 +1,10 @@
 import argparse
-
+from operator import itemgetter
 import torch
+import os
 from torch.autograd import Variable
 
-import data
+import context_data
 
 parser = argparse.ArgumentParser(description='PyTorch Context-filling Language Model')
 
@@ -36,16 +37,102 @@ with open(args.checkpoint, 'rb') as f:
     model = torch.load(f).to(device)
 model.eval()
 
-corpus = data.Corpus(args.data)
+corpus = context_data.Corpus(args.data)
 ntokens = len(corpus.dictionary)
 hidden = model.init_hidden(1)
 
 with torch.no_grad():
-    for line in corpus.context_fill:
+    print("=" * 89)
+    print("============================= Predicting words for test set =============================")
+    print("=" * 89)
+    em=0
+    topV=0
+    topX=0
+    for index, line in enumerate(corpus.test_left):
+        missing_word=[]
         input=torch.LongTensor(line).view(-1,1).to(device)
-        print(input.size())
+        #print(input.size())
         outputs, hidden = model(input, hidden)
-        print(outputs.size(),end="\t")
+        #print(outputs.size(),end="\t")
         output_flat = outputs.view(-1, ntokens)[-1]
-        print(output_flat.size())
-        #k=input()
+        #print(output_flat.size())
+        #print(output_flat)
+
+        for i in range(0,output_flat.size()[-1]):
+            #print(output_flat[i].data, end=", ")
+            if len(missing_word)<10:
+                missing_word.append((i,output_flat[i].data))
+                missing_word.sort(key=itemgetter(1))
+            else:
+                if output_flat[i].data > missing_word[0][1]:
+                    missing_word[0]=(i,output_flat[i].data)
+                    missing_word.sort(key=itemgetter(1))
+
+        #print(missing_word[-5:])
+
+        for w in corpus.test_left[index]:
+            print(corpus.dictionary.idx2word[w],end=" ")
+        print("___",end=" ")
+        for w in corpus.test_right[index]:
+            print(corpus.dictionary.idx2word[w],end=" ")
+
+        print("\nTarget Word: {0}\nCandidate words: ".format(corpus.dictionary.idx2word[corpus.test_target[index]]), end= " ")
+
+        missing_word.reverse() # Reverse list to arrange in descending order of scores
+
+        for idx, _ in missing_word:
+            print(corpus.dictionary.idx2word[idx], end=", ")
+        print("\n")
+
+        predicted_idx = [w[0] for w in missing_word]
+        if predicted_idx[0] == corpus.test_target[index]:
+            em+=1
+
+        if corpus.test_target[index] in predicted_idx[:5]:
+            topV+=1
+
+        if corpus.test_target[index] in predicted_idx[:10]:
+            topX += 1
+
+    print("=" * 80)
+    print("=============================== ACCURACY RESULTS ===============================")
+    print("=" * 80)
+    print("Exact match: {0}/{1} ({2:.2f}%)".format(em, len(corpus.test_target), 100*(em/len(corpus.test_target))))
+    print("Target word in top 5 predicted words: {0}/{1} ({2:.2f}%)".format(topV, len(corpus.test_target), 100*(topV / len(corpus.test_target))))
+    print("Target word in top 10 predicted words: {0}/{1} ({2:.2f}%)".format(topX, len(corpus.test_target), 100*(topX / len(corpus.test_target))))
+    print("=" * 80)
+    print("\n\n\n")
+
+    with open(os.path.join(args.data, "context-fill.txt"), "r") as f:
+        print("=" * 89)
+        print("========================= Predicting words for random sentences =========================")
+        print("=" * 89)
+        for line in corpus.context_left:
+            missing_word=[]
+            input=torch.LongTensor(line).view(-1,1).to(device)
+            #print(input.size())
+            outputs, hidden = model(input, hidden)
+            #print(outputs.size(),end="\t")
+            output_flat = outputs.view(-1, ntokens)[-1]
+            #print(output_flat.size())
+            #print(output_flat)
+
+            for i in range(0,output_flat.size()[-1]):
+                #print(output_flat[i].data, end=", ")
+                if len(missing_word)<5:
+                    missing_word.append((i,output_flat[i].data))
+                    missing_word.sort(key=itemgetter(1))
+                else:
+                    if output_flat[i].data > missing_word[0][1]:
+                        missing_word[0]=(i,output_flat[i].data)
+                        missing_word.sort(key=itemgetter(1))
+
+            #print(missing_word[-5:])
+            print(f.readline(),end="")
+            print("Candidate words: ",end="")
+
+            missing_word.reverse()  # Reverse list to arrange in descending order of scores
+
+            for idx, _ in missing_word:
+                print(corpus.dictionary.idx2word[idx], end=", ")
+            print("\n")
